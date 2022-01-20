c4b4bc31d13cea0679c9ebc6e5e8afb2e996f71a
Fred-Tera-Projetos <98124907+Fred-Tera-Projetos@users.noreply.github.com>
2022-01-20 20:13:42 -0300

    Mudança3

---
+++ js/script.js
@@ -0,0 +1,15 @@
+console.log("Olá!! Projeto Fredericux!!")
+
+let botao = document.getElementById("botao-topo");
+
+window.onscroll = function scrollFunction () {
+  if (document.body.scrollTop > 180 || document.documentElement.scrollTop > 180) {
+    botao.style.display = "block";
+  } else {
+    botao.style.display = "none";
+  }
+}
+
+function voltarParaTopo () {
+  document.documentElement.scrollTop = 0;
+}
