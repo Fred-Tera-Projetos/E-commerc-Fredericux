a1a058fa7be7e013c972ea2369ac4f5e839ec824
Fred-Tera-Projetos <98124907+Fred-Tera-Projetos@users.noreply.github.com>
2022-02-17 18:58:21 -0300

    Add files via upload
    alteração do projeto inicial

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-console.log("Olá!! Projeto Fredericux!!")
+console.log("Olá mundo!");
 
 let botao = document.getElementById("botao-topo");
 
@@ -9,6 +9,41 @@ window.onscroll = function scrollFunction () {
     botao.style.display = "none";
   }
 }
+
+const cep = document.querySelector("#cep");
+
+const options = {
+    method: 'GET',
+    mode: 'cors',
+    cache: 'default'
+}
+
+const showData = (result) => {
+    for (const campo in result){
+        if(document.querySelector("#"+campo)){
+            document.querySelector("#"+campo).value = result[campo];
+        }
+    }
+}
+
+cep.addEventListener("blur", (e) => {
+    let search = cep.value.replace("-", "");
+    console.log(search);
+    
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    .then(response => {
+        response.json()
+        .then(data => {
+            console.log(data)
+            showData(data)
+        })
+    })
+    .catch(e => {
+        console.log("ERRO: "+e)
+    })
+ })
+
+
 
 function voltarParaTopo () {
   document.documentElement.scrollTop = 0;
